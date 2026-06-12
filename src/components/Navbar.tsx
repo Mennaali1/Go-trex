ae2b92fa7eff38dcf2a3fb547636574f7c789bf0
Menna Aly <mennaaly@Mennas-MacBook-Air.local>
2026-06-12 21:58:24 +0300

    changing logo style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,13 +48,13 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
         {/* Logo */}
        <Link href="/" className="flex items-center gap-2 group">
-  <Image
-    src="/logo.jpeg"
-    alt="Go Trex Logo"
-    width={120}
-    height={56}
-    className="object-contain h-12 w-auto brightness-0 invert"
-  />
+ <Image
+  src="/logo.jpeg"
+  alt="Go Trex Logo"
+  width={120}
+  height={56}
+  className="object-contain h-12 w-auto"
+/>
 </Link>
 
         {/* Desktop Nav */}
